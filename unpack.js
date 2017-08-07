825953aa0098097047cc802484befc4b0500efef
_gt3 <gt3.someday@outlook.com>
2017-08-06 23:25:04 -0500

    unlink node_modules in unpack

--- unpack.js
+++ unpack.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 const baltar = require('baltar');
 
 if(fs.existsSync('./runnner/runnner_nm.tgz'))  {
-  console.log('unpack: extracting prebuilt dependencies')
+  if(fs.existsSync('./runnner/node_modules')) {
+    fs.unlinkSync('./runnner/node_modules');
+  }
+  console.log('unpack: extracting prebuilt dependencies');
   const ins = fs.createReadStream('./runnner/runnner_nm.tgz');
   ins.pipe(baltar.unpack('./runnner'));
 }
