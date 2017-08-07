fd4c8e9affa0f45cbb87b2ec7b6a8180b9824dba
Ankit Patel <gt3.someday@outlook.com>
2017-08-06 22:18:56 -0500

    use node remove npx

--- pack.js
+++ pack.js
@@ -3,5 +3,5 @@
 const fs = require('fs');
 const baltar = require('baltar');
 const out = fs.createWriteStream('./runnner/runnner_nm.tgz');
-
+console.log('pack: packing dependencies')
 baltar.pack('./runnner/node_modules').pipe(out);
--- unpack.js
+++ unpack.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const baltar = require('baltar');
 
 if(fs.existsSync('./runnner/runnner_nm.tgz'))  {
+  console.log('unpack: extracting prebuilt dependencies')
   const ins = fs.createReadStream('./runnner/runnner_nm.tgz');
   ins.pipe(baltar.unpack('./runnner'));
 }
